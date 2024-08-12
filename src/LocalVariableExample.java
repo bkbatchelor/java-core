d529ffb94bf5bd46bfaa6506e9108ed4c7abcbed
Brian Batchelor <brian.batchelor@sandboxdev.io>
2024-08-12 11:23:11 -0400

    Adding local variable example

---
+++ src/LocalVariableExample.java
@@ -0,0 +1,22 @@
+public class LocalVariableExample {
+
+    public static void main(String... args){
+
+    }
+
+    private static void lettersOnly(){
+
+    }
+
+    private static void lettersNumbersSymbols(){
+
+    }
+
+    private static void beginWithUnderscore(){
+
+    }
+
+    private static void beginsWithCurrency() {
+
+    }
+}
