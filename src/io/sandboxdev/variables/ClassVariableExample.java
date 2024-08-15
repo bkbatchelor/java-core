3c2daf4cc2471139ff4b62a115b65483aed9803c
Brian Batchelor <brian.batchelor@sandboxdev.io>
2024-08-15 16:37:51 -0400

    <FEATURE> Class variable example

--- src/io/sandboxdev/variables/ClassVariableExample.java
+++ src/io/sandboxdev/variables/ClassVariableExample.java
@@ -1,4 +1,37 @@
 package io.sandboxdev.variables;
 
 public class ClassVariableExample {
+    private static boolean booleanDefault;
+    private static byte byteDefault;
+    private static short shortDefault;
+    private static int intDefault;
+    private static long longDefault;
+
+    public static void main(String... args){
+        booleanDefaultValue();
+        byteDefaultValue();
+        shortDefaultValue();
+        intDefaultValue();
+        longDefaultValue();
+    }
+
+    private static void booleanDefaultValue(){
+        System.out.println(booleanDefault);
+    }
+
+    private static void byteDefaultValue(){
+        System.out.println(byteDefault);
+    }
+
+    private static void shortDefaultValue(){
+        System.out.println(shortDefault);
+    }
+
+    private static void intDefaultValue(){
+        System.out.println(intDefault);
+    }
+
+    private static void longDefaultValue(){
+        System.out.println(longDefault);
+    }
 }
